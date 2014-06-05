b1cdf94bf1d3f6d6d0244a3d43e150cf0af934b8
Shotaro Murai <show.murai@gmail.com>
2014-06-05 15:17:58 +0900

    Added html and python's files.

--- src/weather.py
+++ src/weather.py
@@ -1,9 +1,13 @@
 # -*- coding:utf-8 -*-
 
 import requests
+import json
 
 r = requests.get('http://api.openweathermap.org/data/2.5/weather?q=Tokyo,jp', )
 
 print r.status_code
 print r.headers['content-type']
 print r.json()
+
+encode_json = json.dumps(r.json())
+print encode_json
